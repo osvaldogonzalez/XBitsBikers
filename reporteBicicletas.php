e5ebefa83cd4437b40dea3b4e2b87d503062280a
Osvaldo Gonzalez <osv_gonzalez@hotmail.com>
2017-12-10 18:05:54 -0600

    Manejo de datapicker

--- reporteBicicletas.php
+++ reporteBicicletas.php
@@ -13,6 +13,10 @@
 
         <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
         <script src="js/vendor/jquery-1.11.2.min.js"></script>
+
+        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
+
         <?php session_start(); ?>
     </head>
 
@@ -44,8 +48,9 @@
                     $("#fade").delay(100).fadeOut('slow');
                 });
         }
-
+      
     </script>
+
     <body>
             <div class="header-container">
                     <header class="wrapper clearfix">
@@ -86,18 +91,28 @@
                           ?>
                          </select> <br> 
                             
-                            <label style="margin: auto; width: 20%; padding: 10px;" for="user">Estado:</label>
+                            <label style="margin: auto; width: 40%; padding: 10px;" for="user">Tipo de Evento:</label>
                             <select style="margin: 5px; margin: auto; width: 80%; padding: 10px;" id="Estadobici" name="Estadobici">
                                     <option value="australia">Robada</option>
                                     <option value="australia">Perdida</option>
                                   </select> <br> 
                                   
-                                        <label style="margin: auto; width: 20%; padding: 10px;"for="user">Descripcion del robo:</label>
+                                        <label style="margin: auto; width: 50%; padding: 10px;"for="user">Descripcion del robo:</label>
                                         <input style="margin: 5px; margin: auto; width: 80%; padding: 10px;" id="DescripcionAlerta" name="DescripcionAlerta" required><br>
-                                        <label style="margin: auto; width: 20%; padding: 10px;"for="user">Ubicacion del robo:</label>
+                                        <label style="margin: auto; width: 40%; padding: 10px;"for="user">Ubicacion del robo:</label>
                                         <input style="margin: 5px; margin: auto; width: 80%; padding: 10px;" id="UbicacionRobo" name="UbicacionRobo" required><br>
-                                        <label style="margin: auto; width: 20%; padding: 10px;"for="user">Fecha/Hora del incidente:</label>
-                                        <input style="margin: 5px; margin: auto; width: 80%; padding: 10px;" id="Fecha" name="Fecha" required><br>
+                                        <div class="form-group">
+                                        <label for="dtp_input1" style="margin: auto; width: 50%; padding: 10px;">Fecha/Hora del incidente</label> <br>
+                                        <div class="input-group date form_datetime" data-date="2017-17-01T05:25:07Z" data-date-format="dd MM yyyy - HH:ii p" data-link-field="dtp_input1">
+                                            <input class="form-control" size="16" type="text" value="" readonly>
+                                            <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
+                                            <span class="input-group-addon"><span class="glyphicon glyphicon-th"></span></span>
+                              
+                                        <input type="hidden" id="dtp_input1" value="" /><br/>
+                                    </div>
+                                    </div>
+
+                                       <!-- <input style="margin: 5px; margin: auto; width: 80%; padding: 10px;" id="Fecha" name="Fecha" required>--><br>
                                     
                                       
                                         <input type="submit" style="margin: 20px 0px; width: 200px;"class="button" id="Submit"  name="Submit" value="Reportar" style="margin: 30px 150px;"><br>
@@ -115,7 +130,7 @@
 
         <div class="footer-container">
             <footer class="wrapperfooterfixed">
-                <h3>© Copyright 2017 - Proyecto Registro de Bicicetas 2017</h3>
+                <h3>© Copyright 2017 - Registro de Bicicetas 2017</h3>
                 <h3 style="margin:0px; padding:0px;">© Copyright 2017 - Proyecto Registro de Bicicetas 2017</h3> <a href="index.html" >About XBitsBikers </p>
                     
             </footer>
@@ -125,5 +140,23 @@
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 
         <script src="js/main.js"></script>
+
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap-datetimepicker.js" charset="UTF-8"></script>
+        <script type="text/javascript" src="js/bootstrap-datetimepicker.es.js" charset="UTF-8"></script>
+        <script type="text/javascript">
+
+    $('.form_datetime').datetimepicker({
+        //language:  'fr',
+        weekStart: 1,
+        todayBtn:  1,
+		autoclose: 1,
+		todayHighlight: 1,
+		startView: 2,
+		forceParse: 0,
+        showMeridian: 1
+    });
+    </script>
+
     </body>
 </html>
